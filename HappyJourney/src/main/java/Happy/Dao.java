1bab80d043283562e3da96c74317974da0fb069c
pennantstaff <pennantstaff@PENNANT-VSP106>
2019-11-08 11:49:51 +0530

    from pk

---
+++ HappyJourney/src/main/java/Happy/Dao.java
@@ -0,0 +1,14 @@
+package Happy;
+
+import java.util.List;
+
+
+public interface Dao {
+
+	public int registerUser(UserModel um);
+	public int loginUser(UserModel u);
+	public int loginadmin(UserModel u);
+	public List<BusModel> getBustypes();
+	public List<BusModel> getStation();
+	public List<BusModel> getRegnos();
+}
